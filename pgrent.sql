012a4326a58ce633926e8ad35f329516c49fcd5d
tirth886 <tirth886jain@gmail.com>
2022-11-27 13:13:36 +0530

    booking sql

--- pgrent.sql
+++ pgrent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 20, 2022 at 12:03 PM
+-- Generation Time: Nov 27, 2022 at 08:42 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -20,6 +20,46 @@ SET time_zone = "+00:00";
 --
 -- Database: `pgrent`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `iBookingId` int(11) NOT NULL,
+  `iUserId` int(11) NOT NULL,
+  `iRoomId` int(11) NOT NULL,
+  `fRoomPrice` float(10,2) NOT NULL,
+  `fTotalPrice` float(10,2) NOT NULL,
+  `eStatus` enum('pending','canceled','confirmed') NOT NULL,
+  `ePayType` enum('cash','online','card') NOT NULL DEFAULT 'cash',
+  `iNoPop` int(11) NOT NULL,
+  `dAddedAt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`iBookingId`, `iUserId`, `iRoomId`, `fRoomPrice`, `fTotalPrice`, `eStatus`, `ePayType`, `iNoPop`, `dAddedAt`) VALUES
+(1, 12, 1, 999.99, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:29:59'),
+(2, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:31:36'),
+(3, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:47:39'),
+(4, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:48:54'),
+(5, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:49:08'),
+(6, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:49:09'),
+(7, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:49:17'),
+(8, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:49:42'),
+(9, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:50:07'),
+(10, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:50:19'),
+(11, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:50:20'),
+(12, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 12:50:32'),
+(13, 12, 1, 4455.25, 8910.50, 'pending', 'cash', 2, '2022-11-27 12:50:54'),
+(14, 12, 1, 4455.25, 8910.50, 'pending', 'cash', 2, '2022-11-27 13:02:13'),
+(15, 12, 1, 4455.25, 245038.75, 'pending', 'cash', 55, '2022-11-27 13:08:05'),
+(16, 12, 1, 4455.25, 22276.25, 'pending', 'cash', 5, '2022-11-27 13:11:03');
 
 -- --------------------------------------------------------
 
@@ -77,6 +117,12 @@ INSERT INTO `pg_rooms` (`id`, `vPGname`, `tPGLocation`, `vPGsqfeet`, `vPGRent`, 
 --
 
 --
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`iBookingId`);
+
+--
 -- Indexes for table `mod_admin`
 --
 ALTER TABLE `mod_admin`
@@ -91,6 +137,12 @@ ALTER TABLE `pg_rooms`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `iBookingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `mod_admin`
